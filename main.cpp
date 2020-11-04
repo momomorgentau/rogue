00b41265b5d02604f3ecd79c53aab4710c6b88a2
momomorgentau <sslmatsui0214@gmail.com>
2020-11-04 15:19:21 +0900

    map

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <stdlib.h>
+#include <algorithm>
 using namespace std;
 
 class Map
@@ -33,12 +34,25 @@ public :
 		{
 			_ground[i].resize(_width);
 		}
+		for (int i = 0; i < _height; ++i) 
+		{
+			for (int j = 0; j < _width; ++j)
+			{
+				_ground[i][j] = Route;
+			}
+		}
+	}
 
-		for (auto v : _ground) for (int i : v) i = Wall;
+	//領域外かどうかの判定
+	bool IsOutOfRange(int x, int y) 
+	{
+		if (x <= -1 || _width <= x) return true;
+		if (y <= -1 || _height <= y) return true;
+		return false;
 	}
 
 	//マップを表示
-	void show()
+	void Show()
 	{
 		system("cls");
 		
@@ -52,11 +66,31 @@ public :
 };
 
 
+void Dig(Map map, int x,int y) 
+{
+	//if (map.IsOutOfRange(x, y)) return;
+	int moveX[4] = { 0,0,-1,1};
+	int moveY[4] = {1,-1,0,0};
+	int rand[4] = { 0,1,2,3 };
+	random_shuffle(rand, rand + 4);
+	for (int i = 0; i < 4; ++i) cout << rand[i] << endl;
+}
+
+void DigStart()
+{
+	Map map(10,10);
+	int xStart = 2;
+	int yStart = 4;
+
+	Dig(map, xStart, yStart);
+	map.Show();
+	cout << "hoge" << endl;
+}
+
+
 int main() 
 {
-	Map map(10, 10);
-	map.show();
-	map.show();
+	DigStart();
 
 	return 0;
 }
